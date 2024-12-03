27b40dda7894860bebe05ba45863ec86ba2e3577
Alex Deweert <adeweert@artandlogic.com>
2024-12-03 09:29:42 -0700

    day3 solution

--- index.ts
+++ index.ts
@@ -1,98 +1,93 @@
-import { strict } from "assert";
 import { readFileSync } from "fs";
 
-// Split data into usable array of integers
 const readInput = (filePath: string) => {
   try {
-    const data = readFileSync(filePath, "utf-8").split("\n");
-    const splitData: number[][] = [];
-    data.forEach((d) => splitData.push(d.split(" ").map(x => parseInt(x))));
-    return splitData;
+    const data = readFileSync(filePath, "utf-8");
+    return data;
   } catch (err) {
     console.info(`Error reading file at path ${filePath}: ${err}`);
   }
 };
 
-// With the formatted number, begin the filtering process
-const sd = readInput('./inputs/day2.txt');
-const unsafe: number[][] = [];
+// Match sequences like "mul(123,553)" in capture groups
+// from the input data
+const data = readInput('./inputs/day3.txt');
+const re = /(mul\([0-9]+,[0-9]+\))/g;
+const pairre = /\(([0-9]+),([0-9]+)\)/;
 
-// All increasing
-const strictlyMonotonic = sd.filter(arr => {
-  let monoResult = 0;
-  for(let i = 1; i < arr.length; i++) {
-    const prev = arr[i-1];
-    const cur = arr[i];
-    if(prev > cur) monoResult++;
-    else if(cur > prev) monoResult--;
+const matches = data?.match(re) ?? [];
+let result = 0;
+matches.forEach(a => {
+  // Need to extract the capture groups for just the numbers
+  const pairGroups = a.match(pairre);
+  if(pairGroups) {
+    let num1 = parseInt(pairGroups[1]);
+    let num2 = parseInt(pairGroups[2]);
+    result += num1 * num2;
   }
-  const safe = Math.abs(monoResult) == arr.length-1;
-  if(!safe) unsafe.push(arr);
-  return safe;
 })
 
-// Differ levels
-const differByAtMostThree = strictlyMonotonic.filter(arr => {
-  for(let i = 1; i < arr.length; i++) {
-    const prev = arr[i-1];
-    const cur = arr[i];
-    if(Math.abs(prev-cur) > 3) {
-      unsafe.push(arr);
-      return false;
-    };
-  }
-  return true;
+// Part 1 Result - correct 188741603
+console.log(result);
+
+/**
+ * Second part
+ * Need the first sequence in between ^ and "don't()"
+ * Need only sequences in between "do()" and "don't()"
+ * 
+ * Need the last sequence in between do? and end? or don't and end? :\
+ * 
+ * Brute force, save all strings between start and the first don't()
+ */
+
+const split = data?.split("don't()");
+let result2 = 0;
+
+split?.forEach((a, ai) => {
+  console.log('\n\n------------------------------------------------------');
+  console.log(a);
+  console.log(`~~~`)
+  const dosplit = a.split('do()') ?? [];
+  dosplit.forEach((ds, dsi) => {
+    // The very first ds is ignored since it came after a don't (except for the very first a)
+
+    if(ai == 0 && dsi == 0) {
+      // Do the instructions
+      const submatches = ds?.match(re) ?? [];
+        submatches.forEach(a => {
+          // Need to extract the capture groups for just the numbers
+          const subPairGroups = a.match(pairre);
+          if(subPairGroups) {
+            let num1 = parseInt(subPairGroups[1]);
+            let num2 = parseInt(subPairGroups[2]);
+            result2 += num1 * num2;
+          }
+        })
+      console.log(ds);
+    } else {
+      // If dsi 0, it's directly after a don't - so ignore
+      if(dsi > 0) {
+        console.log(ds);
+        const submatches = ds?.match(re) ?? [];
+        submatches.forEach(a => {
+          // Need to extract the capture groups for just the numbers
+          const subPairGroups = a.match(pairre);
+          if(subPairGroups) {
+            let num1 = parseInt(subPairGroups[1]);
+            let num2 = parseInt(subPairGroups[2]);
+            result2 += num1 * num2;
+          }
+        })
+      }
+    }
+    console.log(`--`)
+  });
+  /**
+   * For each of these, we only want to execute mul instructions that occur after the very first do() - except for the very first one which is already a do.
+   * Does it matter that there are other dos? nah.
+   * 
+   * So, we can split again.
+   */
 });
 
-// differByAtMostThree.forEach(x => console.log(x));
-// console.log(differByAtMostThree.length);
-// console.log(unsafe.length);
-
-// Now we need to take all the unsafe ones, and see if its both strictly monotonic and differ by at most three
-// after removing a bad one.
-
-const sm = (arr: number[]) => {
-  // All increasing
-  let monoResult = 0;
-  for(let i = 1; i < arr.length; i++) {
-    const prev = arr[i-1];
-    const cur = arr[i];
-    if(prev > cur) monoResult++;
-    else if(cur > prev) monoResult--;
-  }
-  const safe = Math.abs(monoResult) == arr.length-1;
-  return safe;
-}
-
-const dbamt = (arr: number[]) => {
-  for(let i = 1; i < arr.length; i++) {
-    const prev = arr[i-1];
-    const cur = arr[i];
-    if(Math.abs(prev-cur) > 3) return false;
-  }
-  return true;
-}
-
-let result = 0;
-unsafe.forEach(arr => {
-  // Each arr is bad - need to remove one char at a time and re-test
-  const copy = [...arr];
-  for(let i = 0; i < arr.length; i++) {
-    // Remove it
-    const removed = arr.splice(i, 1);
-    // console.log(`orig: ${copy}`);
-    // Test what remains
-    // console.log(`removed: ${removed[0]} from ${arr}`)
-    if(sm(arr) && dbamt(arr)) {
-      // console.log(arr);
-      result++
-      break;
-    };
-
-    // Re-insert it
-    arr.splice(i, 0, removed[0]);
-    // console.log(`readded it: ${arr}\n\n`);
-  }
-});
-
-console.log(`Result: ${result + differByAtMostThree.length}`);
+console.log(`Result2: ${result2}`)
