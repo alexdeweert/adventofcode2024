41e917bba12728e341165aa8774f2531eba09dbd
Alex Deweert <adeweert@artandlogic.com>
2024-12-05 09:23:30 -0700

    day5 solution

--- index.ts
+++ index.ts
@@ -1,186 +1,98 @@
 import { readFileSync } from "fs";
 
-const readInput = (filePath: string): string[][] => {
+const readInputPart1 = (filePath: string): Map<string, Array<string>> | undefined => {
+  try {
+    const data = readFileSync(filePath, "utf-8");
+    const splitlines = data.split('\n');
+    const part1map = new Map<string, Array<string>>();
+    splitlines.forEach(line => {
+      const splitrule = line.split('|');
+      const pleft = splitrule[0];
+      const pright = splitrule[1];
+      
+      if(part1map.has(pleft)) part1map.get(pleft)?.push(pright);
+      else part1map.set(pleft, [pright]);
+      // result.push(line.split('|'))
+    });
+    return part1map;
+  } catch (err) {
+    console.info(`Error reading file at path ${filePath}: ${err}`);
+  }
+};
+
+const readInputPart2 = (filePath: string): string[][] | undefined => {
   try {
     const result: string[][] = [];
     const data = readFileSync(filePath, "utf-8");
     const splitlines = data.split('\n');
-    splitlines.forEach(line => result.push(line.split('')));
+    
+    splitlines.forEach(line => {
+      result.push(line.split(','));
+    })
     return result;
   } catch (err) {
     console.info(`Error reading file at path ${filePath}: ${err}`);
   }
-  return [];
 };
-
-// Arr is 140 x 140 chars (total 19600 chars)
-const arr = readInput('./inputs/day4.txt');
-// const arr = [
-//   ["M", "M", "M", "S", "X", "X", "M", "A", "S", "M"],
-//   ["M", "S", "A", "M", "X", "M", "S", "M", "S", "A"],
-//   ["A", "M", "X", "S", "X", "M", "A", "A", "M", "M"],
-//   ["M", "S", "A", "M", "A", "S", "M", "S", "M", "X"],
-//   ["X", "M", "A", "S", "A", "M", "X", "A", "M", "M"],
-//   ["X", "X", "A", "M", "M", "X", "X", "A", "M", "A"],
-//   ["S", "M", "S", "M", "S", "A", "S", "X", "S", "S"],
-//   ["S", "A", "X", "A", "M", "A", "S", "A", "A", "A"],
-//   ["M", "A", "M", "M", "M", "X", "M", "M", "M", "M"],
-//   ["M", "X", "M", "X", "A", "X", "M", "A", "S", "X"]
-// ];
-
-const xmas = 'XMAS';
-
-const checki = (i: number, cur: string) => {
-  return (i == 0 && cur != 'X') || (i == 1 && cur != 'M') || (i == 2 && cur != 'A') || (i == 3 && cur != 'S');
-}
-
-// upleft
-const upleft = (row: number, col: number, acc: string = '', i: number = 0) => {
-  while(row >= 0 && col >= 0 && i < 4) {
-    if(checki(i, arr[row][col])) return false;
-    acc += arr[row][col];
-    row--;
-    col--;
-    i++;
-  }
-  return acc == xmas;
-}
-
-// upright
-const upright = (row: number, col: number, acc: string = '', i: number = 0) => {
-  while(row >= 0 && col < arr[0].length && i < 4) {
-    if(checki(i, arr[row][col])) return false;
-    acc += arr[row][col];
-    row--;
-    col++;
-    i++;
-  }
-  return acc == xmas;
-}
-
-// downleft
-const downleft = (row: number, col: number, acc: string = '', i: number = 0) => {
-  while(row < arr.length && col >= 0 && i < 4) {
-    if(checki(i, arr[row][col])) return false;
-    acc += arr[row][col];
-    row++;
-    col--;
-    i++;
-  }
-  return acc == xmas;
-}
-
-// downright
-const downright = (row: number, col: number, acc: string = '', i: number = 0) => {
-  while(row < arr.length && col < arr[0].length && i < 4) {
-    if(checki(i, arr[row][col])) return false;
-    acc += arr[row][col];
-    row++;
-    col++;
-    i++;
-  }
-  return acc == xmas;
-}
-
-// left
-const left = (row: number, col: number, acc: string = '', i: number = 0) => {
-  while(col >= 0 && i < 4) {
-    if(checki(i, arr[row][col])) return false;
-    acc += arr[row][col];
-    col--;
-    i++;
-  }
-  return acc == xmas;
-}
-
-// right
-const right = (row: number, col: number, acc: string = '', i: number = 0) => {
-  while(col < arr[0].length && i < 4) {
-    if(checki(i, arr[row][col])) return false;
-    acc += arr[row][col];
-    col++;
-    i++;
-  }
-  return acc == xmas;
-}
-
-// up
-const up = (row: number, col: number, acc: string = '', i: number = 0) => {
-  while(row >= 0 && i < 4) {
-    if(checki(i, arr[row][col])) return false;
-    acc += arr[row][col];
-    row--;
-    i++;
-  }
-  return acc == xmas;
-}
-
-// down
-const down = (row: number, col: number, acc: string = '', i: number = 0) => {
-  while(row < arr.length && i < 4) {
-    if(checki(i, arr[row][col])) return false;
-    acc += arr[row][col];
-    row++;
-    i++;
-  }
-  return acc == xmas;
-}
-
-let result = 0;
-for(let row = 0; row < arr.length; row++) {
-  // Each row
-  for(let col = 0; col < arr[0].length; col++) {
-    // From each cur we want to do all the checks (for xmas)
-    // Check upleft, upright, downleft, downright, up, down, left, right - all 8 directions from current
-    if(up(row, col)) result++;
-    if(down(row, col)) result++;
-    if(left(row, col)) result++;
-    if(right(row, col)) result++;
-    if(upleft(row, col)) result++;
-    if(upright(row, col)) result++;
-    if(downleft(row, col)) result++;
-    if(downright(row, col)) result++;
-  }
-}
-
-console.log(`Part 1 Result: ${result}`);
-
 /**
- * Part 2:
+ * For each line in the second section, for each number, just ensure that it follows the rules of the map.
+ * The map will have an entry for each line in the rules, where the value in the rule points to the numbers it needs to come before.
+ * e.g. 75 -> [61,13,24,55]
  * 
- * We need to look at two MAS in the shape of an X
- * Each MAS can be forwards or backwards.
+ * Then, for each line, each number - just ensure that all numbers subsequent to it either aren't in the ruleset or are in the ruleset for tha tnumber.
+ * When does it fail?
  * 
- * M-M
- * -A-
- * S-S
+ * Each number subsequent also needs to be checked to ensure that the prior number isn't before it.
  * 
- * We can see that always in the middle of the block we'll need an A.
- * if we know the upperleft row and col, we know the bounds of the window
- * and we can check that. Can have an "is valid window" fn
+ * We have 23|34
  * 
- * Could do a sliding block of a 3x3 window.
+ * This num 
  */
 
-const isValidWindow = (originRow: number, originCol: number) => {
-  return originRow + 2 < arr.length && originCol + 2 < arr[0].length;
-}
+const part1map = readInputPart1('./inputs/day5part1.txt') ?? new Map<string, Array<string>>();
+const part2data = readInputPart2('./inputs/day5part2.txt') ?? [];
 
-// Then we just iterate across and down, doing the MAS check in a 3x3 grid
-const hasMas = (row: number, col: number) => {
-  const hasDownRight = arr[row][col] == 'M' && arr[row+1][col+1] == 'A' && arr[row+2][col+2] == 'S'
-  const hasUpLeft = arr[row][col] == 'S' && arr[row+1][col+1] == 'A' && arr[row+2][col+2] == 'M'
-  const hasUpRight = arr[row+2][col] == 'M' && arr[row+1][col+1] == 'A' && arr[row][col+2] == 'S'
-  const hasDownLeft = arr[row+2][col] == 'S' && arr[row+1][col+1] == 'A' && arr[row][col+2] == 'M'
-  return (hasDownRight || hasUpLeft) && (hasUpRight || hasDownLeft)
-}
+/**
+ * For each line in part2data, it's valid IF:
+ * 1. Each parent number, considering it's subsequent numbers (sn):
+ *    a. sn either DOESN'T have a map entry (in which case we don't care); or
+ *    b. if it DOES have a map entry, the parent number isn't in it's list
+ */
 
-// Now we need to slide the thing along
-let result2 = 0;
-for(let row = 0; row < arr.length; row++) {
-  for(let col = 0; col < arr[0].length; col++) {
-    if(isValidWindow(row, col) && hasMas(row, col)) result2++;
+let result = 0;
+let invalid: string[][] = [];
+part2data.forEach(line => {
+  let linevalid = true;
+  for(let i = 0; i < line.length; i++) {
+    let parent = line[i];
+    for(let j = i+1; j < line.length; j++) {
+      let sn = line[j];
+      let snValid = !part1map.has(sn) || !part1map.get(sn)!.includes(parent);
+      if(!snValid) {
+        linevalid = false;
+        break;
+      }
+    }
+    if(!linevalid) break;
   }
-}
+  // If line valid, find middle number and break
+  if(linevalid) result += parseInt(line[Math.floor(line.length/2)]);
+  else invalid.push(line.sort((a,b) => {
+      if(part1map.get(a)?.includes(b)) return -1;
+      else if(part1map.get(b)?.includes(a)) return 1;
+      return 0;
+  }));
+});
 
-console.log(`Part 2 Result: ${result2}`);
+console.log(`Result Part1: ${result}`);
+
+/**
+ * Part2: For all incorrectly ordered lines, re-order them according to the page rules and add their middle numbers.
+ * See above part1 sorting for invalid lines.
+ */
+let result2 = 0;
+invalid.forEach(line => {
+  result2 += parseInt(line[Math.floor(line.length/2)]);
+})
+
+console.log(`Result Part2: ${result2}`);
